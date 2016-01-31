f4a9d353495c59da817247fa464e6b8d4688d3c9
830-R2 <R2@830.local>
2016-01-31 13:08:24 -0500

    made drive train

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,13 +1,31 @@
 #include "WPILib.h"
+#include "../wpiutils/830utilities.h"
 
 class Robot: public IterativeRobot
 {
 private:
 
+	static const int FRONT_LEFT_PWM;
+	static const int FRONT_RIGHT_PWM;
+	static const int BACK_LEFT_PWM;
+	static const int BACK_RIGHT_PWM;
+
+	RobotDrive * drive;
+
+	GamepadF310 * pilot;
+	GamepadF310 * copilot;
 
 	void RobotInit()
 	{
+		drive = new RobotDrive(
+			new VictorSP(FRONT_LEFT_PWM),
+			new VictorSP(BACK_LEFT_PWM),
+			new VictorSP(FRONT_RIGHT_PWM),
+			new VictorSP(BACK_RIGHT_PWM)
+		);
 
+		pilot = new GamepadF310(0);
+		copilot = new GamepadF310(1);
 	}
 
 	void AutonomousInit()
@@ -27,7 +45,10 @@ private:
 
 	void TeleopPeriodic()
 	{
+		float left = pilot->LeftY();
+		float right = pilot->RightY();
 
+		drive->TankDrive(left, right, true);
 	}
 
 	void TestPeriodic()
