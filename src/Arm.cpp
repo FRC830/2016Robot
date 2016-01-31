3a59d5e6ff033a0514d89c1dd17830c6cff90234
830-R2 <R2@830.local>
2016-01-31 15:15:05 -0500

    added arm code

--- src/Arm.cpp
+++ src/Arm.cpp
@@ -6,13 +6,32 @@
  */
 
 #include <Arm.h>
+#include <WPILib.h>
+#include "../wpiutils/830utilities.h"
 
 Arm::Arm(Encoder * armEncoder, VictorSP * armMotor) {
 	encoder = armEncoder;
 	arm = armMotor;
+	targetPosition = INTAKE_POSITION;
 }
 
-Arm::~Arm() {
-	// TODO Auto-generated destructor stub
+void Arm::goToIntake(){
+	targetPosition = INTAKE_POSITION;
 }
+
+void Arm::goToShooting(){
+	targetPosition = SHOOTING_POSITION;
+}
+
+void Arm::update(){
+	if( (encoder->Get() - targetPosition) > MARGIN_OF_ERROR){
+		arm->Set(ARM_UP_SPEED);
+	}
+	else if( (encoder->Get() - targetPosition) < -MARGIN_OF_ERROR){
+		arm->Set(ARM_DOWN_SPEED);
+	}
+	arm->Set(0.0);
+}
+
+Arm::~Arm() {}
 
--- src/Arm.h
+++ src/Arm.h
@@ -8,17 +8,26 @@
 #ifndef SRC_ARM_H_
 #define SRC_ARM_H_
 
+#include <WPILib.h>
+#include "../wpiutils/830utilities.h"
+
 class Arm {
 public:
-
 	Arm(Encoder * armEncoder, VictorSP * armMotor);
+	void goToIntake();
+	void goToShooting();
+	void update();
 	virtual ~Arm();
 private:
 	Encoder * encoder;
 	VictorSP * arm;
+	int targetPosition;
 
 	static const int INTAKE_POSITION = 0;
-	static const int SHOOT_POSITION = 60;
+	static const int SHOOTING_POSITION = 60;
+	static const int MARGIN_OF_ERROR = 1;
+	static const int ARM_UP_SPEED = 1.0;
+	static const int ARM_DOWN_SPEED = -0.5;
 };
 
 #endif /* SRC_ARM_H_ */
