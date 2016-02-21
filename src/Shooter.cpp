ef9dd7c68252cc700f2e4722be2668165269e785
830-R1 <R1@830.local>
2016-02-21 13:55:32 -0500

    fixed shooter

--- src/Shooter.cpp
+++ src/Shooter.cpp
@@ -88,7 +88,7 @@ void Shooter::update(){
 			shooter->Set(SHOOT_SPEED);
 			arm->goToShooting();
 			if(timer->Get() >= SHOOT_WAIT_TIME && arm->isAtTargetPosition()){
-				//state = SHOOTING;
+				state = SHOOTING;
 				intake->Set(ROLL_TO_SHOOT_SPEED);
 				timer->Reset();
 				timer->Start();
